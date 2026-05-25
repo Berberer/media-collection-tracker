5e508b30ed7da88cc3f8df1ed04d60a9b1c73f9f
Berberer <lukas.brandt@veds.de>
2026-05-25 16:44:01 +0200

    feat: add import order to linting

--- eslint.config.js
+++ eslint.config.js
@@ -4,6 +4,7 @@ const angular = require('angular-eslint');
 const rxjs = require('@rxlint/eslint-plugin');
 const rxjsAngular = require('@rxlint/eslint-plugin-angular');
 const eslintConfigPrettier = require('eslint-config-prettier');
+const simpleImportSort = require('eslint-plugin-simple-import-sort');
 
 const codeStyle = {
   quotes: [2, 'single', { avoidEscape: true }],
@@ -21,6 +22,8 @@ const codeStyle = {
   '@typescript-eslint/no-namespace': 'off',
   '@typescript-eslint/explicit-function-return-type': 'error',
   '@rxlint/no-unsafe-takeuntil': 'error',
+  'simple-import-sort/imports': 'error',
+  'simple-import-sort/exports': 'error',
 };
 
 const commonExtends = [
@@ -47,6 +50,7 @@ module.exports = tseslint.config(
     processor: angular.processInlineTemplates,
     plugins: {
       '@rxlint-angular': rxjsAngular,
+      'simple-import-sort': simpleImportSort,
     },
     rules: {
       '@angular-eslint/directive-selector': [
@@ -84,6 +88,9 @@ module.exports = tseslint.config(
       parserOptions: {
         projectService: true,
       },
+    },
+    plugins: {
+      'simple-import-sort': simpleImportSort,
     },
     rules: {
       '@typescript-eslint/unbound-method': 'off',
